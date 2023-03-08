0d9847fc15b92430cd868b936a30bfc8e96b64ff
efrenruizrubio <efren282@outlook.es>
2023-03-07 21:51:44 -0600

    :construction: Added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { HomePage, QualifiersPage } from './pages/index'
 
 function App () {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/calificadores' element={<QualifiersPage />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/QualifiersPage.jsx
@@ -0,0 +1,7 @@
+export const QualifiersPage = () => {
+  return (
+    <div>
+      <h1>Calificadores</h1>
+    </div>
+  )
+}
