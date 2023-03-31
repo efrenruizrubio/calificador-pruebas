9e58b5331fc51cb29907c0a0a854dd3319871807
Efrén Ruíz <efren282@outlook.es>
2023-03-31 16:47:08 -0600

    :sparkles: Added a filter to the results component: filter by patient name

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -1,30 +1,46 @@
 import { useState, useEffect } from 'react'
 import { getAll } from '../../services/index'
+import styles from './Result.module.scss'
 
 export const Result = () => {
   const [results, setResults] = useState([])
+  const [filter, setFilter] = useState('')
 
   useEffect(() => {
     getAll().then((res) => setResults(res.data))
   }, [])
+
   return (
-    <div>
-      <h1>Resultados</h1>
-      <ul>
-        {results.length && results.map((result) => {
-          return (
-            <li key={result.id}>
-              <div>
-                <h3>{result.patient.name}</h3>
-                <p>{result.age}</p>
-                <p>{result.score}</p>
-              </div>
+    <section className={styles.result}>
+      <h1 className={styles.result_title}>Resultados</h1>
+      <input
+        type='text'
+        value={filter}
+        placeholder='Buscar por nombre de paciente'
+        className={styles.result_filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
 
-            </li>
-
-          )
-        })}
+      <ul className={styles.result_list}>
+        {results.length &&
+          results
+            .filter((r) =>
+              r.patient.name
+                .toLowerCase()
+                .startsWith(filter.toLocaleLowerCase())
+            )
+            .map((result) => {
+              return (
+                <li key={result.id}>
+                  <div>
+                    <h3>{result.patient.name}</h3>
+                    <p>{result.patient.age}</p>
+                    <p>{result.score}</p>
+                  </div>
+                </li>
+              )
+            })}
       </ul>
-    </div>
+    </section>
   )
 }
